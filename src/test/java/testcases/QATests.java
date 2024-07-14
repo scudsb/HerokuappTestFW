b4ab0e778d03868d5234b1869e98133e6789bfc6
Shama Bharadwaj <scudsb@gmail.com>
2024-07-13 22:15:19 -0400

    Added the required files as an initial setup

--- src/test/java/testcases/QATests.java
+++ src/test/java/testcases/QATests.java
@@ -3,8 +3,6 @@ package testcases;
 import com.aventstack.extentreports.Status;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.herokuapp.pageEvents.*;
-import org.testng.annotations.Test;
 
 
 public class QATests extends baseTest{
